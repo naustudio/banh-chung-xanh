775e6eecbc67dee3609ca0e00085b89e4c1ad105
quy.tran <quy.tran@naustud.io>
2015-01-22 15:30:44 +0700

    integrate FB Login

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1565653007009669',
+    secret: 'e731e75a2d54c874abae17cff2b4a305'
+});
