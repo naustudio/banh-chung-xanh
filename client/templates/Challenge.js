5b033234f287a9b38660e13464b83f605f0aa244
Thanh Tran <trongthanh@gmail.com>
2015-01-27 14:43:21 +0700

    Try updating FB sharing settings

--- client/templates/Challenge.js
+++ client/templates/Challenge.js
@@ -1,13 +1,12 @@
 
 Template.Challenge.events ({
 	'click .button__challenge': function() {
-		FB.ui ({
+		FB.ui({
 			method: 'share',
-			href: 'https://developers.facebook.com/docs/',
-			name: 'Facebook Dialogs',
-			link: 'https://developers.facebook.com/docs/dialogs/',
-			caption: 'Reference Documentation',
-			description: 'Lorem ipsum dolor sit amet, consectetur arum aliquam reprehenderit laudantium disti. Dicta.'
+			href: Meteor.absoluteUrl(''), //share from the root of the app
+			// link: 'https://developers.facebook.com/docs/dialogs/',
+			// caption: 'Reference Documentation',
+			// description: 'Lorem ipsum dolor sit amet, consectetur arum aliquam reprehenderit laudantium disti. Dicta.'
 		}, function(response) {
 			if (response && response.post_id) {
 				console.log('Post was published.');
--- client/templates/Social.js
+++ client/templates/Social.js
@@ -1,19 +1,19 @@
-     /////facebook SDK
-	window.fbAsyncInit = function() {
-		FB.init ({
-			appId      : '1604534863095419', /////must change appId with domain
-			xfbml      : true,
-			version    : 'v2.1'
-		});
-	};
+ /////facebook SDK
+window.fbAsyncInit = function() {
+	FB.init ({
+		appId      : '1604534863095419', /////must change appId with domain
+		xfbml      : true,
+		version    : 'v2.1'
+	});
+};
 
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) {return;}
-		js = d.createElement(s); js.id = id;
-		js.src = '//connect.facebook.net/en_US/sdk.js';
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement(s); js.id = id;
+	js.src = '//connect.facebook.net/en_US/sdk.js';
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 
 
