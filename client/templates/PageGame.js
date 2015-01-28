39be20d34616cdd781d6e0306f7f8878b8b24112
Thanh Tran <trongthanh@gmail.com>
2015-01-28 18:18:56 +0700

    implement temporary keyboard event binding (refactor to a unified input controller is needed)

--- client/templates/PageGame.js
+++ client/templates/PageGame.js
@@ -20,6 +20,44 @@ Template.PageGame.rendered = function() {
 			var steps = game ? game.getNumStep() : 0;
 			Session.set('steps', steps);
 		});
+
+		// bind keyboard events, will move to an input controller
+		$('body').off('keyup keydown').on({
+			'keyup': function(event) {
+				console.log('keyup', event);
+				var direction = '';
+
+				switch (event.keyCode) {
+					case 37:
+						direction = 'Left';
+						break;
+					case 38:
+						direction = 'Up';
+						break;
+					case 39:
+						direction = 'Right';
+						break;
+					case 40:
+						direction = 'Down';
+				}
+
+				if (direction && game) {
+					game['go' + direction]();
+					event.preventDefault();
+				}
+			},
+
+			'keydown': function(event) {
+				if (event.keyCode === 37 ||
+					event.keyCode === 38 ||
+					event.keyCode === 39 ||
+					event.keyCode === 40)
+				{
+					// prevent overflow body to be scrolled by directional key event
+					event.preventDefault();
+				}
+			}
+		});
 	});
 };
 
