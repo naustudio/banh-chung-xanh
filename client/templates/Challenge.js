6738cfda4e4bafc2b8849dabf47e7af4498d39cc
trang <trang.pham@naustud.io>
2015-01-30 18:28:51 +0700

    fix method changllen friends

--- client/templates/Challenge.js
+++ client/templates/Challenge.js
@@ -7,13 +7,24 @@ Template.Challenge.events ({
 		}
 
 		console.log('FB URL to share:', currentUrl);
+		var description = [
+			'Bạn khâm phục thành tích chơi của mình? Vậy hãy thử thách bạn bè của mình để quyên được nhiều tiền hơn cho các em nhỏ.',
+			'Bạn nghĩ chẳng ai có thể chơi giỏi hơn mình? Hãy thách bạn bè cùng chơi để chứng tỏ nào!',
+			'Đây có phải thành tích tốt nhất chưa? Mời bạn bè cùng chơi để xem nhé.',
+			'Không ai có thể giải nhanh hơn bạn? Thách bạn bè nào!'
+		];
+
+		var index = Math.floor((Math.random() * 4));
+
+		var mapId = Router.current().params.mapId;
+		var caption ='Thử thách '+mapId+' - Bánh Chưng Xanh';
 
 		FB.ui({
 			method: 'feed',
 			//href: currentUrl, //share from current URL of the app
 			link: currentUrl,
-			caption: 'Reference Documentation',
-			description: 'Lorem ipsum dolor sit amet, consectetur arum aliquam reprehenderit laudantium disti. Dicta.'
+			caption: caption,
+			description: description[index]
 		}, function(response) {
 			if (response && response.post_id) {
 				console.log('Post was published.');
