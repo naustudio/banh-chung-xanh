25e64698e2fd491b73c069b5d325dbffa2684b15
trang <trang.pham@naustud.io>
2015-02-02 14:51:25 +0700

    add logo, tooltip, font face for congratulation text

--- client/templates/Challenge.js
+++ client/templates/Challenge.js
@@ -6,6 +6,8 @@ Template.Challenge.events ({
 			currentUrl = Meteor.absoluteUrl(currentUrl.substring(1)); //substring to remove the initial /
 		}
 
+		//var currentUrl = document.location.origin;
+
 		console.log('FB URL to share:', currentUrl);
 
 		var description = [i18n('challenge1'), i18n('challenge2'), i18n('challenge3'), i18n('challenge4')];
@@ -13,7 +15,12 @@ Template.Challenge.events ({
 		var index = Math.floor((Math.random() * 4));
 
 		var mapId = Router.current().params.mapId;
+
 		var caption ='Thử thách '+mapId+' - Bánh Chưng Xanh';
+
+		if (typeof(mapId) === 'undefined') {
+			caption = 'Bánh Chưng Xanh by Nau Studio';
+		}
 
 		//console.log(description[index], caption);
 
@@ -30,7 +37,7 @@ Template.Challenge.events ({
 				console.log('Post was not published.');
 			}
 		});
-	},
+	}
 
 
 });
