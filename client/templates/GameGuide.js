d5b62d4a3395a4eef83c3d82e4c2f8c931ed1b2c
trang <trang.pham@naustud.io>
2015-02-04 09:47:38 +0700

    fix layout

--- client/templates/GameGuide.js
+++ client/templates/GameGuide.js
@@ -35,6 +35,6 @@ Template.GameGuide.helpers({
 	heightSlider: function() {
 		var newHeight = $('#slider ul li:first-child').height();
 		$('#slider').height(newHeight);
-		return newHeight?newHeight:'400px';
+		return '400px';
 	}
 });
