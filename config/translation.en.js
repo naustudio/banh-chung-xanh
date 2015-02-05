7689865c6ec11c616979b7aaf56861c6aba2f4c5
trang <trang.pham@naustud.io>
2015-02-05 17:50:16 +0700

    fix layout, add copy translate

--- config/translation.en.js
+++ config/translation.en.js
@@ -3,12 +3,14 @@
  */
 /*global i18n*/
 i18n.map('en', {
+
 	'nav_home': 'Home',
-	'nav_intro': 'Introduction',
-	'nav_instructions': 'Instructions',
-	'nav_sponsors': 'Sponsors',
-	'nav_donate': 'Donate',
-	'nav_players': 'Players',
+	'nav_intro': 'Project Introduction',
+	'nav_instructions': 'Game Play',
+	'nav_sponsors': 'Sponsor List',
+	//'nav_donate': 'Donate',
+	'nav_players': 'Player List',
+
 	'players_title': 'Players List',
 	'players_name': 'Name',
 	'players_levels': 'Levels',
@@ -24,11 +26,18 @@ i18n.map('en', {
 	'cancel': 'Cancel',
 	'register': 'Register',
 	'or': 'Or',
-	'shareAndSavePoints' :'Share and save points',
+	'shareAndSavePoints' :'Please log in to record the donation progress.',
 	'fillEmail': 'Fill in email',
 	'fillName': 'Fill in name',
 	'hello': 'Hello',
 	'project_purpose': 'The purpose of Project',
+	'project_purpose_description': 'Hello, Banh Chung Xanh is a charity game project of NAU Studio to introduce and spread the spirit of helping people to others.\
+					When you join the game, you have made a very great contribution to NAU\'s charitable fund.\
+					NAU will give this money to children with disabilities, orphans at Mai Am Thien Phuc, Cu Chi district.\
+					The more player, the more money sent to them. So, what are you considering?',
+	'project_purpose_call': 'Join and share Banh Chung Xanh!',
+	'project_purpose_sologan_1': 'Play for kindness',
+	'project_purpose_sologan_2': ' ',
 	'guide': 'Guide',
 	'play': 'Play',
 	'round': 'Round',
@@ -38,25 +47,45 @@ i18n.map('en', {
 	'emailError': 'Please enter your email address',
 	'requiredError': 'This is requried',
 	'privacyRequiredError': 'Please accept terms & condition.',
-	'challenge1': 'Bạn khâm phục thành tích chơi của mình? Vậy hãy thử thách bạn bè của mình để quyên được nhiều tiền hơn cho các em nhỏ.',
-	'challenge2': 'Bạn nghĩ chẳng ai có thể chơi giỏi hơn mình? Hãy thách bạn bè cùng chơi để chứng tỏ nào!',
-	'challenge3': 'Đây có phải thành tích tốt nhất chưa? Mời bạn bè cùng chơi để xem nhé.',
-	'challenge4': 'Không ai có thể giải nhanh hơn bạn? Thách bạn bè nào!',
+
+	'challenge1': 'Are you faster than me? Show me what you got!',
+	'challenge2': 'Do you admire my score? Not? Ok, try it.',
+	'challenge3': 'Do you play faster than me? Show me!',
+	'challenge4': 'Is this not the best score? Prove it!',
+	'challenge5': 'Show me you can play!',
+	'challenge6': 'You say you can play. Try it!',
+
+	'description-step-1': 'Trên đây là bản đồ 10 vòng chơi của Bánh Chưng Xanh, bao gồm 3 cấp độ dễ, bình thường, và khó được xếp ngẫu nhiên. \
+				Mỗi cấp độ sẽ tương ứng với một số tiền quyên góp. Bạn có thể chọn chơi bất kì vòng nào bạn thích.',
+	'description-step-2': 'Mục tiêu của trò chơi này là bạn phải giúp nhân vật anh Nâu đẩy hết bánh chưng vào từng chiếc đĩa có cắm cờ. Anh Nâu chỉ có thể đi lên, đi xuống, đi sang trái và đi sang phải. \
+				Bạn có thể dùng bàn phím, chuột hay tay nhấn vào 4 mũi tên chỉ phương hướng của nhân vật.',
+	'description-step-3': 'Bạn có thể đi lại bước vừa đi bằng cách bấm vào nút mũi tên đậm phía trên bản đồ. Hoặc là đi lại từ đầu bằng cách nhấn nút vòng tròn mũi tên.',
+	'description-step-4': 'Bạn sẽ nhìn thấy bảng thông tin số tiền đã được đóng góp cho đến lúc này, thứ tự vòng bạn đang chơi và số bước đi của bạn trong vòng chơi này.',
+	'description-step-5': 'Khi chiếc bánh chưng được đẩy vào chiếc đĩa, nó sẽ đổi màu. Và khi bạn đẩy hết bánh chưng vào đĩa, bảng thông báo Chúc mừng sẽ được hiện lên. \
+				Chúc mừng bạn đã hoàn thành vòng chơi! Bạn có thể nhấn nút chơi tiếp để đến vòng tiếp theo, hoặc nhấn vào nút bản đồ để quay lại bản đồ của trò chơi.',
+
+
 	'tooltip_map': 'Master map',
 	'tooltip_reset': 'Reset',
 	'tooltip_back': 'Back',
 	'congratulation': 'Congratulation',
 	'master-map': 'Master map',
 	'next': 'Next',
+
 	'congratulation-description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-	'congratulation-description-no-login': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lets login',
+	'congratulation-description-start': 'Congratulation! You have finished this round and contributed',
+	'congratulation-description-end': 'to Banh Chung Xanh\'s charity fund.',
+	'congratulation-description-no-login': 'Congratulation! You have finished this round. \
+		You need to log in for us to record the result and donate to NAU\'s charity fund. Thank you so much.',
+
 	'translate': 'Tiếng Việt',
 	'date-left': 'Date left',
-	'sponsor-description': 'Total of funds',
+	'sponsor-description': 'Contributed to the total of',
 	'number-step': 'Steps',
 	'privacy-policy': 'Privacy Policy',
-	'agree': 'I agree',
-	'of-website': '',
-	'copyright': 'All Rights Reserved'
+	'agree': 'I agree with',
+	'of-website': ' ',
+	'copyright': 'All Rights Reserved',
+	'policy-link': '/policy.en.html'
 
 });
--- config/translation.vi.js
+++ config/translation.vi.js
@@ -3,12 +3,14 @@
  */
 /*global i18n*/
 i18n.map('vi', {
+
 	'nav_home': 'Trang chủ',
-	'nav_intro': 'Giới thiệu campaign',
+	'nav_intro': 'Giới thiệu dự án',
 	'nav_instructions': 'Hướng dẫn cách chơi',
 	'nav_sponsors': 'Danh sách nhà hảo tâm',
-	'nav_donate': 'Hướng dẫn cách đóng góp',
+	//'nav_donate': 'Hướng dẫn cách đóng góp',
 	'nav_players': 'Danh sách người tham gia',
+
 	'players_title': 'Danh sách người chơi',
 	'players_name': 'Quý danh',
 	'players_levels': 'Số vòng',
@@ -24,11 +26,17 @@ i18n.map('vi', {
 	'cancel': 'Hủy',
 	'register': 'Đăng ký',
 	'or': 'Hoặc',
-	'shareAndSavePoints' :'Để chia sẻ là lưu điểm',
+	'shareAndSavePoints' :'Vui lòng đăng nhập để chúng tôi lưu lại kết quả quyên góp.',
 	'fillEmail': 'Điền email',
 	'fillName': 'Điền tên',
 	'hello': 'Xin chào',
 	'project_purpose': 'Mục đích dự án',
+	'project_purpose_description': 'Xin chào, Bánh Chưng Xanh là một dự án game từ thiện của NÂU Studio nhằm lan tỏa tinh thần thiện nguyện của các bạn.\
+						Khi tham gia chơi game, các bạn đã "lập lệnh chuyển" một số tiền từ quỹ từ thiện có sẵn do các đơn vị tài trợ đến trẻ em khuyết tật, mồ côi ở mái ấm Thiên Phúc,\
+						huyện Củ Chi mà không cần quyên góp bằng tiền của mình. Càng nhiều người chơi, càng vượt được nhiều vòng, số tiền được gửi đến các em càng lớn.',
+	'project_purpose_call': 'Hãy tham gia chơi và chia sẻ ngay!',
+	'project_purpose_sologan_1': 'Chơi Game - Thêm việc thiện',
+	'project_purpose_sologan_2': 'Chia sẻ - Thêm niềm vui',
 	'guide': 'Xem hướng dẫn',
 	'play': 'Không cần, chơi luôn!',
 	'round': 'Vòng',
@@ -38,18 +46,36 @@ i18n.map('vi', {
 	'emailError': 'Vui lòng nhập email',
 	'requiredError': 'Thông tin này bắt buộc nhập',
 	'privacyRequiredError': 'Vui lòng chọn đồng ý điều khoản sử dụng',
-	'challenge1': 'Bạn khâm phục thành tích chơi của mình? Vậy hãy thử thách bạn bè của mình để quyên được nhiều tiền hơn cho các em nhỏ.',
-	'challenge2': 'Bạn nghĩ chẳng ai có thể chơi giỏi hơn mình? Hãy thách bạn bè cùng chơi để chứng tỏ nào!',
-	'challenge3': 'Đây có phải thành tích tốt nhất chưa? Mời bạn bè cùng chơi để xem nhé.',
-	'challenge4': 'Không ai có thể giải nhanh hơn bạn? Thách bạn bè nào!',
+
+	'challenge1': 'Thử xem bạn nhanh hơn tôi không?',
+	'challenge2': 'Bạn khâm phục thành tích chơi của tôi không? Không thì thử đi.',
+	'challenge3': 'Bạn chơi nhanh hơn tôi không? Thử nhé',
+	'challenge4': 'Đây chưa phải thành tích tốt nhất? Bạn thử xem!',
+	'challenge5': 'Bạn giải nhanh hơn tôi? Thử xem.',
+	'challenge6': 'Bạn có phải dạng vừa không? Thử xem nào!',
+
+	'description-step-1': 'Trên đây là bản đồ 10 vòng chơi của Bánh Chưng Xanh, bao gồm 3 cấp độ dễ, bình thường, và khó được xếp ngẫu nhiên. \
+				Mỗi cấp độ sẽ tương ứng với một số tiền quyên góp. Bạn có thể chọn chơi bất kì vòng nào bạn thích.',
+	'description-step-2': 'Mục tiêu của trò chơi này là bạn phải giúp nhân vật anh Nâu đẩy hết bánh chưng vào từng chiếc đĩa có cắm cờ. Anh Nâu chỉ có thể đi lên, đi xuống, đi sang trái và đi sang phải. \
+				Bạn có thể dùng bàn phím, chuột hay tay nhấn vào 4 mũi tên chỉ phương hướng của nhân vật.',
+	'description-step-3': 'Bạn có thể đi lại bước vừa đi bằng cách bấm vào nút mũi tên đậm phía trên bản đồ. Hoặc là đi lại từ đầu bằng cách nhấn nút vòng tròn mũi tên.',
+	'description-step-4': 'Bạn sẽ nhìn thấy bảng thông tin số tiền đã được đóng góp cho đến lúc này, thứ tự vòng bạn đang chơi và số bước đi của bạn trong vòng chơi này.',
+	'description-step-5': 'Khi chiếc bánh chưng được đẩy vào chiếc đĩa, nó sẽ đổi màu. Và khi bạn đẩy hết bánh chưng vào đĩa, bảng thông báo Chúc mừng sẽ được hiện lên. \
+				Chúc mừng bạn đã hoàn thành vòng chơi! Bạn có thể nhấn nút chơi tiếp để đến vòng tiếp theo, hoặc nhấn vào nút bản đồ để quay lại bản đồ của trò chơi.',
+
 	'tooltip_map': 'Chọn thử thách',
 	'tooltip_reset': 'Chơi lại',
 	'tooltip_back': 'Lùi lại',
 	'congratulation': 'Xin chúc mừng',
 	'master-map': 'Chọn thử thách',
 	'next': 'Chơi tiếp',
+
 	'congratulation-description': 'Chúc mừng bạn đã chiến thắng, chúng tôi sẽ quyên góp số tiền là {$1} vào quỹ từ thiện',
-	'congratulation-description-no-login': 'Xin chúc mừng Xin chúc mừng Xin chúc mừng. Hãy đăng nhập để pla pla...',
+	'congratulation-description-start': 'Chúc mừng bạn đã hoàn thành vòng chơi này. Bạn đã đóng góp',
+	'congratulation-description-end': 'vào quỹ từ thiện của NÂU.',
+	'congratulation-description-no-login': 'Chúc mừng bạn đã hoàn thành vòng chơi này. \
+				Tuy nhiên, bạn cần đăng nhập để chúng tôi ghi nhận lại kết quả chơi và đóng góp vào quỹ từ thiện của NÂU. Cám ơn bạn.',
+
 	'translate': 'English',
 	'date-left': 'Ngày còn lại',
 	'sponsor-description': 'Được đóng góp trên tổng',
@@ -57,6 +83,7 @@ i18n.map('vi', {
 	'privacy-policy': 'Điều khoản sử dụng',
 	'agree': 'Tôi đồng ý với',
 	'of-website': 'của trang web',
-	'copyright': 'Giữ toàn quyền'
+	'copyright': 'Giữ toàn quyền',
+	'policy-link': '/policy.vi.html'
 
 });
