9590cf896ae69073d9232b1d01d6f73ced01c27d
Thanh Tran <trongthanh@gmail.com>
2015-02-07 15:28:50 +0700

    Redo dynamic metadata injection using meteor package:  inject-initial to properly generate the metadata from serverside

--- shared/router.js
+++ shared/router.js
@@ -3,36 +3,16 @@
  *
  * Implement router here
  */
+/*jshint multistr:true*/
 /*global i18n, ga*/
-var currentLang;
-if (Meteor.isClient) {
-	currentLang = Session.get('language');
-}
 
 Router.configure({
 	layoutTemplate: 'ApplicationLayout',
 	onAfterAction: function() {
-
-		// meta data
-		var mapId = this.params.mapId;
-		var title = i18n('bcx') + ' - Nau Studio';
-		if (mapId) {
-			title = i18n('round') + ' ' + mapId + ' - ' + title;
+		if (Meteor.isClient) {
+			// use this hook to track page view
+			ga('send', 'pageview', this.url);
 		}
-		var seoData = { title: title };
-
-		if (currentLang !== Session.get('language')) {
-			currentLang = Session.get('language');
-			seoData.meta = {
-				'description': i18n('meta-description')
-			};
-		}
-		console.log('onAfterAction:', seoData);
-
-		SEO.set(seoData);
-
-		// use this hook to track page view
-		ga('send', 'pageview', this.url);
 	}
 });
 
@@ -116,12 +96,14 @@ function applyLanguage(route) {
 		url = url.replace(langParam, lang);
 		console.log('new url', url);
 		route.redirect(url);
+		return;
 	}
 
 	if (Meteor.isClient) {
 		Session.set('language', lang);
 		i18n.setLanguage(lang);
 	}
+
 	return lang;
 }
 
@@ -142,5 +124,60 @@ function getPreferredLanguage() {
 	}
 
 	return preferredLanguage;
+}
+
+/**
+ * Dynamic metadata for social network and SEO
+ */
+/*global Package, WebApp*/
+if (Meteor.isServer) {
+	var handleServeRoute = function(lang, mapId) {
+		// console.log('dynamic head:', req.url);
+		// meta data
+		lang = lang || 'vi';
+		i18n.setLanguage(lang);
+
+		var title = i18n('bcx') + ' - Nau Studio';
+		if (mapId) {
+			title = i18n('round') + ' ' + mapId + ' - ' + title;
+		}
+
+		Inject.rawHead('title',
+			'<title>' + title + '</title>\
+			<meta property="og:title" content="' + title + '" />\
+			<meta name="twitter:title" content="' + title + '">'
+		);
+
+		var metaDescription = i18n('meta-description');
+
+		Inject.rawHead('metaDesc',
+			'<meta name="description" content="' + metaDescription + '>\
+			<meta property="og:description" content="' + metaDescription + '" />\
+			<meta name="twitter:description" content="' + metaDescription + '">'
+		);
+
+
+	};
+
+	// per-resquest handler for inject-initial plugin
+	// https://github.com/meteorhacks/meteor-inject-initial
+	if (!Package.appcache) {
+		var rAppPath = /(\/vi|\/en)(?:\/map)?(\/\d+)?/;
+		WebApp.connectHandlers.use(function(req, res, next) {
+			if (Inject.appUrl(req.url)) {
+				var url = req.url;
+				var matches = url.match(rAppPath);
+
+				if (url === '/' || matches) {
+					var lang = matches && matches[1] && matches[1].substr(1);
+					var mapId = matches && matches[2] && matches[2].substr(1);
+					handleServeRoute(lang, mapId);
+				}
+
+			}
+			next();
+		});
+	}
+
 }
 
