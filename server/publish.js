bfff76a6414b1a0787259c593313f227f3561f7c
quy.tran <quy.tran@naustud.io>
2015-02-10 13:04:24 +0700

    protect credential data. remove autopublish.

--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,4 @@
+/*global Sponsors, Settings*/
 // implement collection publish here, below is example from todos app
 
 // Meteor.publish('publicLists', function() {
@@ -20,8 +21,33 @@
 
 Meteor.publish("userData", function() {
 	// if (this.userId) {
-	return Meteor.users.find({}, {fields: {'gameScores': 1, 'lastAccess': 1}});
+	return Meteor.users.find({}, {fields: {
+								'gameScores': 1,
+								'lastAccess': 1,
+								'username': 1,
+								'services': 1,
+								'profile': 1
+							}});
 	// } else {
 	// 	this.ready();
 	// }
+});
+
+Meteor.publish("sponsors", function() {
+	return Sponsors
+			.find({}, {fields: {
+							'name': 1,
+							'amount': 1,
+							'date': 1,
+							'entryDate': 1
+						}});
+});
+
+Meteor.publish("settings", function() {
+	var includingKeys = ['remainingDate', 'donatedAmount', 'totalAmount'];
+	return Settings
+			.find({key: {$in: includingKeys}}, {fields: {
+							'key': 1,
+							'value': 1
+						}});
 });
